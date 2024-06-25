2841135ec4085c1ae17d1d03315fb809ce07f087
BVK_LMZ <78233321+BVK-LMZ@users.noreply.github.com>
2024-06-24 22:54:28 -0400

    ye
    ye

--- Assets/_Source/StartScreen.cs
+++ Assets/_Source/StartScreen.cs
@@ -5,16 +5,6 @@ using UnityEngine.SceneManagement;
 // Define the StartScreen class
 public class StartScreen : MonoBehaviour
 {
-    // Serialized field for the button GameObject in the Unity Editor
-    [SerializeField] private GameObject buttonObject; // Serialized field for the button GameObject
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Ensure the button object is active at the start
-        buttonObject.SetActive(true); // Activate the button GameObject
-    }
-
     // Update is called once per frame
     void Update()
     {
@@ -25,18 +15,10 @@ public class StartScreen : MonoBehaviour
             LoadMainGame();
         }
     }
-
-    // Method to be called by the button click event
-    public void OnButtonClick()
-    {
-        // Call method to load the MainGame scene
-        LoadMainGame();
-    }
-
     // Method to load the MainGame scene
     private void LoadMainGame()
     {
         // Load the MainGame scene
-        SceneManager.LoadScene("MainGame");
+        SceneManager.LoadScene("MainMenu");
     }
 }
